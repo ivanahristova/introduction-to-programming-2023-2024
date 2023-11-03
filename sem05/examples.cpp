9d80cda2e290357ab4bba0d42f7efad5465dd8cc
Ivana Hristova <ivanahristova8@gmail.com>
2023-11-03 07:09:19 +0200

    Add materials for seminar 05

---
+++ sem05/examples.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstring>
+
+void example01() {
+    int number = 5;
+    int* ptrToNumber = &number;// Създаваме променлива от тип указател към int с име ptrToNumber, в която съхраняваме адреса на number
+
+    std::cout << &number << " == " << ptrToNumber << std::endl;
+
+    std::cout << &ptrToNumber << std::endl;
+
+    (*ptrToNumber)++; // приоритетът е важен
+    std::cout << number << std::endl;
+}
+
+void example02() {
+    bool* trickOrTreatWon = nullptr;
+    bool treat = true;
+    trickOrTreatWon = &treat;
+    std::cout << "trickOrTreatWon [" << trickOrTreatWon << "]: " << *trickOrTreatWon << std::endl;
+
+    bool* trickOrTreatLost = trickOrTreatWon; // Двата пойнтъра имат една и съща стойност
+    std::cout << "trickOrTreatLost [" << trickOrTreatLost << "]: " << *trickOrTreatLost << std::endl;
+
+    *trickOrTreatWon = false;
+    std::cout << "trickOrTreatWon [" << trickOrTreatWon << "]: " << *trickOrTreatWon << std::endl;
+    std::cout << "trickOrTreatLost [" << trickOrTreatLost << "]: " << *trickOrTreatLost << std::endl;
+
+    bool trick = true;
+    std::cout << "treat [" << &treat << "]: " << treat << std::endl;
+    std::cout << "trick [" << &trick << "]: " << trick << std::endl;
+
+    treat = &trickOrTreatLost;
+    std::cout << "treat [" << &treat << "]: " << treat << std::endl;
+    std::cout << "trick [" << &trick << "]: " << trick << std::endl;
+
+    *trickOrTreatWon = 1;
+    std::cout << "trickOrTreatWon [" << trickOrTreatWon << "]: " << *trickOrTreatWon << std::endl;
+    std::cout << "trickOrTreatLost [" << trickOrTreatLost << "]: " << *trickOrTreatLost << std::endl;
+
+    *trickOrTreatLost = !(*trickOrTreatWon);
+    std::cout << "trickOrTreatWon [" << trickOrTreatWon << "]: " << *trickOrTreatWon << std::endl;
+    std::cout << "trickOrTreatLost [" << trickOrTreatLost << "]: " << *trickOrTreatLost << std::endl;
+}
+
+void myFunction(int* ptr) {
+    std::cout << "Pointer version" << std::endl;
+}
+
+void myFunction(int value) {
+    std::cout << "Non-pointer version" << std::endl;
+}
+
+void example03() {
+    myFunction(nullptr);
+    myFunction(NULL);
+}
+
+int main() {
+    // example01();
+    // example02();
+    // example03();
+
+    // const int a = 5;
+    // int const b = 6;
+
+    // std::cout << a << ' ' << b << std::endl;
+    // a++;
+    // --b;
+
+}
