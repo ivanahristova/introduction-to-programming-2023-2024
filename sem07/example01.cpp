b4e88c0ef08215e351cc6ca7a8aab54dc308ae88
Ivana Hristova <ivanahristova8@gmail.com>
2023-11-17 06:52:23 +0200

    Add material for seminar 07

---
+++ sem07/example01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+double f(double d = 6.0){
+    double &e = d;
+    e += 7.0;
+    return d;
+}
+
+int main() {
+    std::cout << f() << std::endl;
+    std::cout << f(8) << std::endl;
+}
