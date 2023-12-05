018a903093c6605ca5d7953c7a4135952b3f2f95
Ivana Hristova <ivanahristova8@gmail.com>
2023-12-05 13:31:55 +0200

    Add problems for seminar 10

---
+++ sem10/solution04.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// Да се напише функция, която по подадена матрица създава нейна огледална версия
+
+void printMatrix(int** matrix, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int** createMatrix(size_t rows, size_t cols) {
+    int** matrix = new int*[rows];
+
+    for (size_t i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+        for (size_t j = 0; j < cols; j++) {
+            matrix[i][j] = i*j;
+        }
+    }
+
+    return matrix;
+}
+
+int main() {
+    size_t rows = 5, cols = 5;
+    int** matrix = createMatrix(rows, cols);
+    printMatrix(matrix, rows, cols);
+
+
+}
